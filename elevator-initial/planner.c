b51702156092997a988a0b50a648a242122f818f
alex <alex>
2017-12-12 18:31:08 +0100

    harcoded stoping distance, sorting works

--- elevator-initial/planner.c
+++ elevator-initial/planner.c
@@ -71,7 +71,7 @@ void insertTarget(s32 stoppingDistance, s32 currentPosition, s32 data, int (*com
 void requestPosition(s32 data){
   s32 currentPosition = getCarPosition();
   s32 duty = getMotorCurrentDuty(); // Probably 200 duty per 1cm/s. Check motor.h 
-  s32 stoppingDistance = 2 * duty / 200; // duty / 200 -> cm's per second. 2 == senconds given to stop
+  s32 stoppingDistance = 5;//TODO remove hardcoded 2 * duty / 200; // duty / 200 -> cm's per second. 2 == senconds given to stop
   int (*cmprFun[2]) (const void*, const void*);
   cmprFun[0] = cmpLessThan;
   cmprFun[1] = cmpGreaterThan;
@@ -83,12 +83,17 @@ void requestPosition(s32 data){
 u8 setNextTarget(){
   if (helper.numberOfTargets > 0){
     setCarTargetPosition(helper.targets[0]);
-    helper.numberOfTargets--;
-    memmove(&(helper.targets), &(helper.targets[1]), helper.targetLength-1);
-    helper.targets[helper.targetLength-1] = 0; 
     return 1;
   }
   return 0;
+}
+        
+void removeTarget(){
+  if (helper.numberOfTargets > 0){
+    helper.numberOfTargets--;
+    memmove(&(helper.targets), &(helper.targets[1]), sizeof(s32)*helper.targetLength-1);
+    helper.targets[helper.targetLength-1] = 0; 
+  }
 }
 
 void decideDirection(s32 carPosition){
@@ -115,7 +120,10 @@ static void plannerTask(void *params) {
         break;
       case(ARRIVED_AT_FLOOR) :
         decideDirection(getCarPosition());
-
+        //Check that we not only arive at floor but also have stoped
+        //Also, we will assume that this is the first target in targets
+        //and remove the first taget. If this is not the case we are screwed.
+        removeTarget(); 
         // TODO wait 1s before setting this flag to 1..
         floorSwitch = 1;
         break;
@@ -124,7 +132,7 @@ static void plannerTask(void *params) {
       default:
         break;
     }
-    if(floorSwitch && setNextTarget()) floorSwitch = 0;
+    setNextTarget();
   }
 }
 
